330a1c223de55105465615053075e86c610ec52f
HaaaToka <okanalan97.14@gmail.com>
2020-05-29 03:20:06 +0300

    glove tries

--- PoemGenerator/main.py
+++ PoemGenerator/main.py
@@ -1,5 +1,10 @@
 #import dynet
 import json
+from glove import Glove
+
+
+gg = Glove(cooccurence=[1,2]).load_stanford("glovo/glove.6B.50d.txt")
+
 
 with open('unim_poem.json') as json_file:
     data = json.load(json_file)
