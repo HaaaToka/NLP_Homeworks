e1e0c44f613639c77235c5e04f898eb945df8f55
HaaaToka <okanalan97.14@gmail.com>
2020-05-29 23:24:56 +0300

    reading file

--- PoemGenerator/main.py
+++ PoemGenerator/main.py
@@ -1,13 +1,39 @@
 #import dynet
 import json
 from glove import Glove
+import numpy as np
+import csv
 
 
-gg = Glove(cooccurence=[1,2]).load_stanford("glovo/glove.6B.50d.txt")
 
+def read_dataset(fpath):
+    print("Reading dataset...")
+    with open(fpath) as json_file:
+        data = json.load(json_file)
+        for d in data:
+            print(d["poem"])
+            break
 
-with open('unim_poem.json') as json_file:
-    data = json.load(json_file)
-    for d in data:
-        print(d["poem"])
+def read_glove(fpath):
+    print('Loading word vectors...')
+    word2vec = {}
+    embeds = []
+    word2idx = {}
+    with open(fpath, encoding='utf-8') as f:
+      for line in f:
+        values = line.split()
+        word = values[0]
+        word2idx[word] = len(embeds)
+        vec = np.asarray(values[1:], dtype='float32')
+        word2vec[word] = vec
+        embeds.append(vec)
         break
+    return np.array(embeds),word2idx
+
+
+
+
+
+read_dataset('unim_poem.json')
+embedding,w2i = read_glove('glovo/glove.6B.50d.txt')
+print(embedding)
