67e95a9be4a7065e4693527c3a74872f3506de3e
okan <okan@jotform.com>
2020-06-25 11:49:05 +0300

    final works

--- midtermStudies/funtions.py
+++ midtermStudies/funtions.py
@@ -1,6 +1,14 @@
 import math
 
+def derivateofsigmoid(sigvalue):
+    return sigvalue*(1-sigvalue)
+
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
-print(sigmoid(-1.43))
+x=(0.9*4.5)+(0.17*-5.2)+(-2)
+
+sig = sigmoid(x)
+dsig = derivateofsigmoid(sig)
+
+print("sigmoid of {} = {} and \nDerivate of this sigmoid {} = {}".format(x,sig,sig,dsig))
